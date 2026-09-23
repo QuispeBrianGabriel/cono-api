3ff7332c68b2ec62a649d965ba6b0232fe34ba99
QuispeBrianGabriel <quispebriangabriel@gmail.com>
2026-09-23 23:43:59 +0000

    refactor: user response impl from user

--- src/main/java/com/cono/api/contracts/UserResponse.java
+++ src/main/java/com/cono/api/contracts/UserResponse.java
@@ -1,5 +1,10 @@
 package com.cono.api.contracts;
 
+import com.cono.api.model.User;
 import java.util.UUID;
 
-public record UserResponse(UUID id, String name, String surname, String email) {}
+public record UserResponse(UUID id, String name, String surname, String email) {
+  public static UserResponse from(User user) {
+    return new UserResponse(user.getId(), user.getName(), user.getSurname(), user.getEmail());
+  }
+}
--- src/main/java/com/cono/api/controller/UserController.java
+++ src/main/java/com/cono/api/controller/UserController.java
@@ -7,12 +7,11 @@ import com.cono.api.service.UserService;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,12 +21,10 @@ public class UserController {
   private final UserService serv;
 
   @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest req) {
-    User user = serv.createUser(req.name(), req.surname(), req.email(), req.password());
-    URI location = URI.create("/users/" + user.getId());
-    UserResponse res =
-        new UserResponse(user.getId(), user.getName(), user.getSurname(), user.getEmail());
-    return ResponseEntity.created(location).body(res);
+    User us = serv.createUser(req.name(), req.surname(), req.email(), req.password());
+    URI location = URI.create("/users/" + us.getId());
+    UserResponse resp = UserResponse.from(us);
+    return ResponseEntity.created(location).body(resp);
   }
 }
