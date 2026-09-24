c36ecef062581bedd6256199ac0508ca85445532
QuispeBrianGabriel <quispebriangabriel@gmail.com>
2026-09-24 16:16:39 +0000

    chore: coverage in 100%

--- src/main/java/com/cono/api/ApiApplication.java
+++ src/main/java/com/cono/api/ApiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
+import lombok.Generated;
+
+@Generated
 @SpringBootApplication
 @EnableJdbcAuditing
 public class ApiApplication {
