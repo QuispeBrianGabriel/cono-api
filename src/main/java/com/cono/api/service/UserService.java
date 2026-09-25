f6afefbe8907e22e9ab448269ed53561ace9d0e4
QuispeBrianGabriel <quispebriangabriel@gmail.com>
2026-09-25 18:46:06 +0000

    func: encoder password

--- src/main/java/com/cono/api/service/UserService.java
+++ src/main/java/com/cono/api/service/UserService.java
@@ -3,15 +3,18 @@ package com.cono.api.service;
 import com.cono.api.model.User;
 import com.cono.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
   public User createUser(String name, String surname, String email, String password) {
-    User user = User.builder().name(name).surname(surname).email(email).password(password).build();
+    String hashed = passwordEncoder.encode(password);
+    User user = User.builder().name(name).surname(surname).email(email).password(hashed).build();
     return userRepository.save(user);
   }
 }
